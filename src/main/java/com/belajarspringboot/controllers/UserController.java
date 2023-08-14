a89e58e8b729e2707974f917516f6cb80ae01c14
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-14 10:29:50 +0700

    add get alluser without pass

--- src/main/java/com/belajarspringboot/controllers/UserController.java
+++ src/main/java/com/belajarspringboot/controllers/UserController.java
@@ -1,15 +1,20 @@
 package com.belajarspringboot.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.belajarspringboot.dto.DataUserWithoutPass;
 import com.belajarspringboot.dto.ResponseData;
 import com.belajarspringboot.models.entities.User;
 import com.belajarspringboot.services.UserService;
@@ -22,8 +27,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping
-    public ResponseEntity<ResponseData<User>> tambah(@Valid @RequestBody User user, Errors errs){
+    @PostMapping("/register")
+    public ResponseEntity<ResponseData<User>> register(@Valid @RequestBody User user, Errors errs){
         ResponseData<User> dataResponse=new ResponseData<>();
         dataResponse.setStatus(false);
          dataResponse.setPayload(null);
@@ -38,7 +43,7 @@ public class UserController {
          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dataResponse);
         }
         try {
-            User savedUser=userService.Tambah(user);
+            User savedUser=userService.register(user);
             dataResponse.setStatus((true));
             dataResponse.setPayload(savedUser);
             dataResponse.getMessages().add("berhasil");
@@ -48,5 +53,18 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dataResponse);
         }
        
+    }
+    @GetMapping
+    public ResponseEntity<List<DataUserWithoutPass>> getAllUser(){
+        DataUserWithoutPass datauser = new DataUserWithoutPass();
+        List<DataUserWithoutPass> datausers = new ArrayList<DataUserWithoutPass>();
+        Iterable<User> users = userService.getAllUser();
+        for (User user : users) {
+            datauser.setId(user.getId());
+            datauser.setNama(user.getNama());
+            datauser.setEmail(user.getEmail());
+            datausers.add(datauser);
+        }
+        return ResponseEntity.ok(datausers);
     }
 }
---
+++ src/main/java/com/belajarspringboot/dto/DataUserWithoutPass.java
@@ -0,0 +1,10 @@
+package com.belajarspringboot.dto;
+
+import lombok.Data;
+
+@Data
+public class DataUserWithoutPass {
+    private Integer id;
+    private String nama;
+    private String email;
+}
